afbf68cf15a885f7152a5f54a686e4f42e22fe04
suhai-art <thegui4000@gmail.com>
2026-07-09 23:46:13 -0300

    fix<app/boostrap>: remove exceptions returns

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,14 +23,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn(Request $request) => $request->is('*'),
         );
-
-        $exceptions->render(function (Throwable $e, Request $request) {
-            $status = FormatApiResponse::resolveExceptionStatus($e);
-
-            return response()->json(
-                FormatApiResponse::resolveExceptionPayload($e, $status),
-                $status
-            );
-        });
     })
     ->create();
